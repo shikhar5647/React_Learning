f1a3c93226eba50df74ad2c917b66c54fd9916c5
shikhar5647 <b22ch032@iitj.ac.in>
2025-01-17 01:53:38 +0530

    default_props

--- text_utils/src/components/Navbar.js
+++ text_utils/src/components/Navbar.js
@@ -1,10 +1,12 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 
-export default function Navbar() {
+
+export default function Navbar(props) {
   return (
     <nav className="navbar navbar-expand-lg bg-body-tertiary">
         <div className="container-fluid">
-          <a className="navbar-brand" href="/">Textutils_Shikhar</a>
+          <a className="navbar-brand" href="/">{props.title}</a>
           <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
           </button>
@@ -29,3 +31,7 @@ export default function Navbar() {
       </nav>
   )
 }
+
+Navbar.propTypes={title: PropTypes.string}
+
+Navbar.defaultProps={title: "Set title here"}
