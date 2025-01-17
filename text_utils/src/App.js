ec78c94f591ad6f80aaaf570d4faf445eb844ff7
shikhar5647 <b22ch032@iitj.ac.in>
2025-01-17 20:31:38 +0530

    added_style

--- text_utils/src/App.js
+++ text_utils/src/App.js
@@ -2,6 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
+import About from './components/About';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Navbar title="TextUtils_by_Shikhar"/>
       <div className="container my-3">
         <TextForm heading="Enter the text to analyze"/>
+        <About/>
       </div>
     </>
   );
