9387d0e9e4311fb529ad99ecf7af2d6a88360ea1
shikhar5647 <b22ch032@iitj.ac.in>
2025-01-17 15:20:13 +0530

    better_logic

--- text_utils/src/components/TextForm.js
+++ text_utils/src/components/TextForm.js
@@ -12,12 +12,18 @@ export default function TextForm(props) {
     }   
     const [text,setText]=useState('Enter text here');
     return (
-        <div>
+        <>
+        <div className="container">
             <h1>{props.heading}</h1>
             <div className="mb-3">
                 <textarea classname="form-control" value={text} onChange={handleOnChange} id="myBox" rows="8"></textarea>
             </div>
             <button className='btn btn-primary' onClick={handleUpClick}>Convert to Uppercase</button>
         </div>
+        <div className="container my-3">
+            <h1>Your text summary</h1>
+            <p>{text.split(" ").length} words and {text.length} characters</p>
+        </div>    
+        </>
   )
 }
