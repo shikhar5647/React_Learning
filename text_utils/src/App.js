ce89d236d82ab7ba131e1c54a71a9c512637262f
shikhar5647 <b22ch032@iitj.ac.in>
2025-01-19 12:35:47 +0530

    navbar_dark

--- text_utils/src/App.js
+++ text_utils/src/App.js
@@ -3,11 +3,13 @@ import './App.css';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
 import About from './components/About';
+import { useState } from 'react';
 
 function App() {
+  const [mode,setMode]=useState('light'); //whether dark mode is enabled or not
   return (
     <>
-      <Navbar title="TextUtils_by_Shikhar"/>
+      <Navbar title="TextUtils_by_Shikhar" mode={modeode} toggleMode={setMode}/>
       <div className="container my-3">
         <TextForm heading="Enter the text to analyze"/>
         <About/>
