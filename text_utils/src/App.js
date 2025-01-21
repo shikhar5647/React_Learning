be503e037a20330f0a17a427de07da63e350fc6b
shikhar5647 <b22ch032@iitj.ac.in>
2025-01-21 23:32:21 +0530

    initial_alert

--- text_utils/src/App.js
+++ text_utils/src/App.js
@@ -3,6 +3,7 @@ import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
 import About from './components/About';
 import { useState } from 'react';
+import Alert from './components/Alert';
 
 function App() {
   const [mode, setMode] = useState('light'); // whether dark mode is enabled or not
@@ -24,6 +25,7 @@ function App() {
         mode={mode} 
         toggleMode={toggleMode}
       />
+      <Alert alert="This is alert" />
       <div className="container my-3">
         <TextForm 
           heading="Enter the text to analyze" 
