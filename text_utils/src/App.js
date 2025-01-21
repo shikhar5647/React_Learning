8bce75d8b187629afb14ea2061a4cbf82f8c6bb6
shikhar5647 <b22ch032@iitj.ac.in>
2025-01-21 23:01:28 +0530

    dark_throughout

--- text_utils/src/App.js
+++ text_utils/src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import './App.css';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
@@ -6,16 +5,35 @@ import About from './components/About';
 import { useState } from 'react';
 
 function App() {
-  const [mode,setMode]=useState('light'); //whether dark mode is enabled or not
+  const [mode, setMode] = useState('light'); // whether dark mode is enabled or not
+
+  const toggleMode = () => {
+    if (mode === 'light') {
+      setMode('dark');
+      document.body.style.backgroundColor = '#042743';
+    } else {
+      setMode('light');
+      document.body.style.backgroundColor = 'white';
+    }
+  }
+
   return (
     <>
-      <Navbar title="TextUtils_by_Shikhar" mode={mode} toggleMode={setMode}/>
+      <Navbar 
+        title="TextUtils_by_Shikhar" 
+        mode={mode} 
+        toggleMode={toggleMode}
+      />
       <div className="container my-3">
-        <TextForm heading="Enter the text to analyze"/>
-        <About/>
+        <TextForm 
+          heading="Enter the text to analyze" 
+          mode={mode}
+        />
+        <About mode={mode}/>
       </div>
     </>
   );
 }
 
 export default App;
+
--- text_utils/src/components/Navbar.js
+++ text_utils/src/components/Navbar.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
-
 export default function Navbar(props) {
   return (
     <nav className={`navbar navbar-expand-lg bg-body-tertiary bg-${props.mode} navbar-${props.mode}`}>
@@ -22,16 +21,23 @@ export default function Navbar(props) {
                 <a className="nav-link active" aria-current="page" href="/">About</a>
               </li>
             </ul>
-            <form className="d-flex" role="search">
-              <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
-              <button className="btn btn-outline-success" type="submit">Search</button>
-            </form>
+            <div className={`form-check form-switch text-${props.mode==='light'?'dark':'light'}`}>
+              <input className="form-check-input" onClick={props.toggleMode} type="checkbox" role="switch" id="flexSwitchCheckDefault"/>
+              <label className="form-check-label" htmlFor="flexSwitchCheckDefault">Enable Dark Mode</label>
+            </div>
           </div>
         </div>
       </nav>
   )
 }
 
-Navbar.propTypes={title: PropTypes.string}
+Navbar.propTypes = {
+  title: PropTypes.string,
+  mode: PropTypes.string,
+  toggleMode: PropTypes.func
+}
 
-Navbar.defaultProps={title: "Set title here"}
+Navbar.defaultProps = {
+  title: "Set title here",
+  mode: "light"
+}
