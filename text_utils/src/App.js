2003795b48faa0463152eac43b74e5265fd81c5d
shikhar5647 <b22ch032@iitj.ac.in>
2025-01-21 23:41:53 +0530

    alert_show

--- text_utils/src/App.js
+++ text_utils/src/App.js
@@ -7,14 +7,27 @@ import Alert from './components/Alert';
 
 function App() {
   const [mode, setMode] = useState('light'); // whether dark mode is enabled or not
+  const [alert, setAlert] = useState(null);
+
+  const showAlert = (message, type) => {
+    setAlert({
+      msg: message,
+      type: type
+    })
+    setTimeout(() => {
+      setAlert(null);
+    }, 1500);
+  }
 
   const toggleMode = () => {
     if (mode === 'light') {
       setMode('dark');
       document.body.style.backgroundColor = 'grey';
+      showAlert("Dark mode has been enabled", "success");
     } else {
       setMode('light');
       document.body.style.backgroundColor = 'white';
+      showAlert("Light mode has been enabled", "success");
     }
   }
 
@@ -25,7 +38,7 @@ function App() {
         mode={mode} 
         toggleMode={toggleMode}
       />
-      <Alert alert="This is alert" />
+      <Alert alert={alert} />
       <div className="container my-3">
         <TextForm 
           heading="Enter the text to analyze" 
--- text_utils/src/components/Alert.js
+++ text_utils/src/components/Alert.js
@@ -4,7 +4,7 @@ function Alert(props) {
   return (
     <div>
       <div className="alert alert-warning alert-dismissible fade show" role="alert">
-        <strong>{props.alert}</strong>
+        <strong>{props.alert.type}</strong> : {props.alert.msg}
         <button type="button" className="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
       </div>
     </div>
