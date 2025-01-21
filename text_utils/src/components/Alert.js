6b3c1a2a3a3d17ee9aba2a41578ca4d7cf83558e
shikhar5647 <b22ch032@iitj.ac.in>
2025-01-22 01:57:10 +0530

    debugged

--- text_utils/src/components/Alert.js
+++ text_utils/src/components/Alert.js
@@ -1,14 +1,15 @@
-import React from 'react'
+import React from 'react';
 
 function Alert(props) {
   return (
-    <div>
-      <div className="alert alert-warning alert-dismissible fade show" role="alert">
-        <strong>{props.alert.type}</strong> : {props.alert.msg}
+    props.alert && (
+      <div className={`alert alert-${props.alert.type} alert-dismissible fade show`} role="alert">
+        <strong>{props.alert.type.charAt(0).toUpperCase() + props.alert.type.slice(1)}</strong>: {props.alert.msg}
         <button type="button" className="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
       </div>
-    </div>
-  )
+    )
+  );
 }
 
-export default Alert
+export default Alert;
+
