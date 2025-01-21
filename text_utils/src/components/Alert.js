2804c3ec9022c770ff10e60cad19096305361d33
shikhar5647 <b22ch032@iitj.ac.in>
2025-01-22 02:00:43 +0530

    alerted

--- text_utils/src/components/Alert.js
+++ text_utils/src/components/Alert.js
@@ -5,7 +5,6 @@ function Alert(props) {
     props.alert && (
       <div className={`alert alert-${props.alert.type} alert-dismissible fade show`} role="alert">
         <strong>{props.alert.type.charAt(0).toUpperCase() + props.alert.type.slice(1)}</strong>: {props.alert.msg}
-        <button type="button" className="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
       </div>
     )
   );
