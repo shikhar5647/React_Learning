8105d4cc44f4c63ce1968cb25e357e2632b7f5cc
shikhar5647 <b22ch032@iitj.ac.in>
2025-01-24 00:43:08 +0530

    router

--- text_utils/src/App.js
+++ text_utils/src/App.js
@@ -4,6 +4,13 @@ import TextForm from './components/TextForm';
 import About from './components/About';
 import { useState } from 'react';
 import Alert from './components/Alert';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link
+} from "react-router-dom";
+
 
 function App() {
   const [mode, setMode] = useState('light'); // whether dark mode is enabled or not
